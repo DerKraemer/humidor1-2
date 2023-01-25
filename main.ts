4b720c6f8eb25b88d75da50fd5c6c7fab7e174ed
DerKraemer <122726071+DerKraemer@users.noreply.github.com>
2023-01-25 18:55:14 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,41 +1,4 @@
-let Anfeuchten = false
-let FMag = 0
-let FZig = 0
-let Stundenzähler = 0
-let MLüfter = 0
-let ULüfter = 0
-let Blinker = 0
-let Sekundentimer = 0
-let Stundentimer = false
-let Stundentimermerker = 0
-let Tageszähler = 1
-let AnzahlAnfeuchten = 0
-let Anfeuchtzähler = 0
-oledssd1306.initDisplay()
-oledssd1306.setTextXY(2, 0)
-oledssd1306.writeString("Magazin=     %")
-oledssd1306.setTextXY(3, 0)
-oledssd1306.writeString("Feuchte=     %")
-oledssd1306.setTextXY(5, 0)
-oledssd1306.writeString(" M=AUS   U=AUS")
-// Entspricht 0,5 0,5 0.5 Stunde
-// 
-loops.everyInterval(1800000, function () {
-    Stundentimer = !(Stundentimer)
-    Stundenzähler += 1
-    if (Stundenzähler > 48) {
-        Stundenzähler = 0
-        Tageszähler += 1
-        AnzahlAnfeuchten = Anfeuchtzähler / Tageszähler
-    }
-})
-loops.everyInterval(2000, function () {
-    Sekundentimer = 1
-})
-// Motoransteuerung
-basic.forever(function () {
-    oledssd1306.setTextXY(0, 0)
-    oledssd1306.writeNumber(Anfeuchtzähler)
+function fkt_Feuchtemessung () {
     // Feuchtemessung und Displayausgabe
     if (Sekundentimer) {
         Sekundentimer = 0
@@ -49,18 +12,34 @@ basic.forever(function () {
         oledssd1306.writeNumber(FMag)
         oledssd1306.setTextXY(7, 0)
         if (FMag < 75) {
-            if (!(Blinker)) {
+            if (Blinker) {
                 oledssd1306.writeString("Magazin füllen!")
-                Blinker = 1
             } else {
                 oledssd1306.clearRange(15)
-                Blinker = 0
             }
         } else {
             oledssd1306.clearRange(15)
-            Blinker = 0
         }
     }
+}
+function fkt_Anfeuchtzähler () {
+    oledssd1306.setTextXY(6, 7)
+    if (Blinker) {
+        oledssd1306.writeNumber(Anfeuchtzähler)
+    } else {
+        oledssd1306.clearRange(15)
+    }
+}
+function fkt_Motoransteuerung_Umluft () {
+    // Umluftsteuerung
+    if (Stundentimer && !(ULüfter)) {
+        motors.dualMotorPower(Motor.B, 100)
+        ULüfter = 1
+        oledssd1306.setTextXY(5, 11)
+        oledssd1306.writeString("AN ")
+    }
+}
+function fkt_Motorsteuerung_Feuchte () {
     // Motoransteuerung feuchte
     if (FZig < 68) {
         Anfeuchten = true
@@ -91,11 +70,52 @@ basic.forever(function () {
             oledssd1306.writeString("AUS")
         }
     }
-    // Umluftsteuerung
-    if (Stundentimer && !(ULüfter)) {
-        motors.dualMotorPower(Motor.B, 100)
-        ULüfter = 1
-        oledssd1306.setTextXY(5, 11)
-        oledssd1306.writeString("AN ")
+}
+let Stundenzähler = 0
+let Anfeuchten = false
+let FMag = 0
+let FZig = 0
+let Anfeuchtzähler = 0
+let Stundentimer = false
+let Sekundentimer = 0
+let Blinker = false
+let ULüfter = 0
+let MLüfter = 0
+MLüfter = 0
+ULüfter = 0
+Blinker = false
+Sekundentimer = 0
+Stundentimer = false
+let Stundentimermerker = 0
+let Tageszähler = 1
+let AnzahlAnfeuchten = 0
+Anfeuchtzähler = 0
+oledssd1306.initDisplay()
+oledssd1306.setTextXY(2, 0)
+oledssd1306.writeString("Magazin=     %")
+oledssd1306.setTextXY(3, 0)
+oledssd1306.writeString("Feuchte=     %")
+oledssd1306.setTextXY(5, 0)
+oledssd1306.writeString(" M=AUS   U=AUS")
+// Entspricht 0,5 0,5 0.5 Stunde
+// 
+loops.everyInterval(1800000, function () {
+    Stundentimer = !(Stundentimer)
+    Stundenzähler += 1
+    if (Stundenzähler > 48) {
+        Stundenzähler = 0
+        Tageszähler += 1
+        AnzahlAnfeuchten = Anfeuchtzähler / Tageszähler
     }
+})
+loops.everyInterval(2000, function () {
+    Sekundentimer = 1
+    Blinker = !(Blinker)
+})
+// Motoransteuerung
+basic.forever(function () {
+    fkt_Anfeuchtzähler()
+    fkt_Feuchtemessung()
+    fkt_Motorsteuerung_Feuchte()
+    fkt_Motoransteuerung_Umluft()
 })
