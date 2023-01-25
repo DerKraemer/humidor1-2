b050a084f0fdbc6d4f6895f7f96ad53578853c7b
DerKraemer <122726071+DerKraemer@users.noreply.github.com>
2023-01-25 15:49:17 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,74 +1,53 @@
+let Anfeuchten = false
 let FMag = 0
 let FZig = 0
+let Stundenzähler = 0
 let MLüfter = 0
 let ULüfter = 0
 let Blinker = 0
 let Sekundentimer = 0
-let Stundentimer = 0
+let Stundentimer = false
+let Stundentimermerker = 0
+let Tageszähler = 1
+let AnzahlAnfeuchten = 0
+let Anfeuchtzähler = 0
 oledssd1306.initDisplay()
-oledssd1306.setTextXY(0, 0)
-oledssd1306.writeString("Magazin=")
-oledssd1306.setTextXY(0, 13)
-oledssd1306.writeString("%")
 oledssd1306.setTextXY(2, 0)
-oledssd1306.writeString("Feuchte=")
-oledssd1306.setTextXY(2, 13)
-oledssd1306.writeString("%")
+oledssd1306.writeString("Magazin=     %")
 oledssd1306.setTextXY(3, 0)
-oledssd1306.writeString("Magazin:")
-oledssd1306.setTextXY(4, 0)
-oledssd1306.writeString("Umlüftung:")
+oledssd1306.writeString("Feuchte=     %")
+oledssd1306.setTextXY(5, 0)
+oledssd1306.writeString(" M=AUS   U=AUS")
+// Entspricht 0,5 0,5 0.5 Stunde
+// 
+loops.everyInterval(1800000, function () {
+    Stundentimer = !(Stundentimer)
+    Stundenzähler += 1
+    if (Stundenzähler > 48) {
+        Stundenzähler = 0
+        Tageszähler += 1
+        AnzahlAnfeuchten = Anfeuchtzähler / Tageszähler
+    }
+})
 loops.everyInterval(2000, function () {
     Sekundentimer = 1
 })
 // Motoransteuerung
 basic.forever(function () {
-    // Umluftsteuerung
-    if (Stundentimer && !(ULüfter)) {
-        motors.dualMotorPower(Motor.B, 100)
-        ULüfter = 1
-        oledssd1306.setTextXY(4, 11)
-        oledssd1306.writeString("AN ")
-    }
-    // Motoransteuerung feuchte
-    if (FZig < 68) {
-        if (!(MLüfter)) {
-            motors.dualMotorPower(Motor.A, 100)
-            MLüfter = 1
-            oledssd1306.setTextXY(3, 11)
-            oledssd1306.writeString("AN ")
-        }
-        if (!(ULüfter)) {
-            motors.dualMotorPower(Motor.B, 100)
-            ULüfter = 1
-            oledssd1306.setTextXY(4, 11)
-            oledssd1306.writeString("AN ")
-        }
-    }
-    if (FZig > 72) {
-        motors.dualMotorPower(Motor.A, 0)
-        MLüfter = 0
-        oledssd1306.setTextXY(3, 11)
-        oledssd1306.writeString("AUS")
-        if (!(Stundentimer)) {
-            motors.dualMotorPower(Motor.B, 0)
-            ULüfter = 0
-            oledssd1306.setTextXY(4, 11)
-            oledssd1306.writeString("AUS")
-        }
-    }
+    oledssd1306.setTextXY(0, 0)
+    oledssd1306.writeNumber(Anfeuchtzähler)
     // Feuchtemessung und Displayausgabe
     if (Sekundentimer) {
         Sekundentimer = 0
         DHT11.setPin(DigitalPin.C17)
         FZig = DHT11.humidity()
-        oledssd1306.setTextXY(2, 10)
+        oledssd1306.setTextXY(3, 10)
         oledssd1306.writeNumber(FZig)
         DHT11.setPin(DigitalPin.C16)
         FMag = DHT11.humidity()
-        oledssd1306.setTextXY(0, 10)
+        oledssd1306.setTextXY(2, 10)
         oledssd1306.writeNumber(FMag)
-        oledssd1306.setTextXY(6, 0)
+        oledssd1306.setTextXY(7, 0)
         if (FMag < 75) {
             if (!(Blinker)) {
                 oledssd1306.writeString("Magazin füllen!")
@@ -82,13 +61,41 @@ basic.forever(function () {
             Blinker = 0
         }
     }
-})
-// Entspricht 1 Stunde
-// 
-loops.everyInterval(5000, function () {
-    if (Stundentimer) {
-        Stundentimer = 0
-    } else {
-        Stundentimer = 1
+    // Motoransteuerung feuchte
+    if (FZig < 68) {
+        Anfeuchten = true
+        if (!(MLüfter)) {
+            Anfeuchtzähler += 1
+            motors.dualMotorPower(Motor.A, 100)
+            MLüfter = 1
+            oledssd1306.setTextXY(5, 3)
+            oledssd1306.writeString("AN ")
+        }
+        if (!(ULüfter)) {
+            motors.dualMotorPower(Motor.B, 100)
+            ULüfter = 1
+            oledssd1306.setTextXY(5, 11)
+            oledssd1306.writeString("AN ")
+        }
+    }
+    if (FZig > 72) {
+        Anfeuchten = false
+        motors.dualMotorPower(Motor.A, 0)
+        MLüfter = 0
+        oledssd1306.setTextXY(5, 3)
+        oledssd1306.writeString("AUS")
+        if (!(Stundentimer)) {
+            motors.dualMotorPower(Motor.B, 0)
+            ULüfter = 0
+            oledssd1306.setTextXY(5, 11)
+            oledssd1306.writeString("AUS")
+        }
+    }
+    // Umluftsteuerung
+    if (Stundentimer && !(ULüfter)) {
+        motors.dualMotorPower(Motor.B, 100)
+        ULüfter = 1
+        oledssd1306.setTextXY(5, 11)
+        oledssd1306.writeString("AN ")
     }
 })
